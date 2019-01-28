0e9917849a970f207d1ac45cb420bb942ff12435
R. Richards <rdrrichards@hotmail.com>
2019-01-28 17:09:10 -0600

    srating domain model

---
+++ QIQO.Monitor.Domain/Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QIQO.Monitor.Domain
+{
+    public class Server
+    {
+        public Server(string serverName, string serverSource)
+        {
+            ServerName = serverName;
+            ServerSource = serverSource;
+        }
+        public string ServerName { get; }
+        public string ServerSource { get; }
+    }
+}
---
+++ QIQO.Monitor.Domain/SqlServer.cs
@@ -0,0 +1,24 @@
+﻿namespace QIQO.Monitor.Domain
+{
+    public class SqlServer : Server
+    {
+        public SqlServer(string serverName, string serverSource) : base(serverName, serverSource)
+        {
+            InstanceName = TryGetInstance(serverSource);
+        }
+        public string InstanceName { get; }
+        private string TryGetInstance(string serverSource)
+        {
+            var nameArray = serverSource.Split("\\");
+            return nameArray.Length > 1 ? nameArray[1] : string.Empty;
+        }
+    }
+    public class Monitor
+    {
+        public string Name { get; }
+    }
+    public class SqlServerMonitor : Monitor
+    {
+        public string QueryText { get; }
+    }
+}
