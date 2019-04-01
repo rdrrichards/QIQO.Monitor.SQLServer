de8f3bdf0ed2e083e660e170bcf2a2908fcd3ff7
R. Richards <rdrrichards@hotmail.com>
2019-04-01 14:37:06 -0500

    api manager unit tests

---
+++ QIQO.Monitor.Tests/ApiManagerUnitTest.cs
@@ -0,0 +1,63 @@
+﻿using Moq;
+using QIQO.Monitor.Api;
+using QIQO.Monitor.Api.Services;
+using QIQO.Monitor.SQLServer.Data;
+using System.Collections.Generic;
+using Xunit;
+
+namespace QIQO.Monitor.Tests
+{
+    public class ApiManagerUnitTest
+    {
+        [Fact]
+        public void EnvironmentManagerTest()
+        {
+            // Arrange
+            var cache = new Mock<ICoreCacheService>();
+            var envData = new List<EnvironmentData> { new EnvironmentData { EnvironmentKey = 1, EnvironmentName = "Test" } };
+            cache.Setup(m => m.GetEnviroments()).Returns(envData);
+
+            // Act
+            var sut = new EnvironmentManager(cache.Object);
+            var envs = sut.GetEnvironments();
+
+            // Assert
+            Assert.True(envs.Count == 1);
+        }
+        [Fact]
+        public void ServiceManagerTest()
+        {
+            // Arrange
+            var cache = new Mock<ICoreCacheService>();
+            var qm = new Mock<IQueryEntityService>();
+            var em = new Mock<IEnvironmentEntityService>();
+            var envData = new List<ServiceData> { new ServiceData { ServiceKey = 1, ServiceName = "Test", InstanceName = "I1",
+                ServerKey = 1, ServiceSource = "Test", ServiceTypeKey = 1 } };
+            cache.Setup(m => m.GetServices()).Returns(envData);
+
+            // Act
+            var sut = new ServiceManager(cache.Object, qm.Object, em.Object);
+            var envs = sut.GetServices();
+
+            // Assert
+            Assert.True(envs.Count == 1);
+        }
+        [Fact]
+        public void ServerManagerTest()
+        {
+            // Arrange
+            var cache = new Mock<ICoreCacheService>();
+            var qm = new Mock<IQueryEntityService>();
+            var em = new Mock<IEnvironmentEntityService>();
+            var envData = new List<ServerData> { new ServerData { ServerKey = 1, ServerName = "Test" } };
+            cache.Setup(m => m.GetServers()).Returns(envData);
+
+            // Act
+            var sut = new ServerManager(cache.Object, qm.Object, em.Object);
+            var envs = sut.GetServers();
+
+            // Assert
+            Assert.True(envs.Count == 1);
+        }
+    }
+}
