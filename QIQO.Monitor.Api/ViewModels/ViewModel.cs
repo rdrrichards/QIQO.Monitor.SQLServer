dbe1816ac4606d8f8ede649a9c72a70a976b5abf
R. Richards <rdrrichards@hotmail.com>
2019-04-09 14:49:43 -0500

    view model updates

--- QIQO.Monitor.Api/ViewModels/ViewModel.cs
+++ QIQO.Monitor.Api/ViewModels/ViewModel.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections.Generic;
-
-namespace QIQO.Monitor.Api
+﻿namespace QIQO.Monitor.Api
 {
     public class ServerAdd
     {
         public string ServerName { get; set; }
-        public List<ServiceAdd> Services { get; set; } = new List<ServiceAdd>();
-        public List<EnvironmentAdd> Environments { get; set; } = new List<EnvironmentAdd>();
+        public int[] Services { get; set; }
+        public int[] Environments { get; set; }
     }
     public class ServiceAdd
     {
         public string ServiceName { get; set; }
         public string InstanceName { get; set; }
         public string ServiceSource { get; set; }
+        public int ServiceTypeKey { get; set; }
+        public int ServerKey { get; set; }
+        public int[] Environments { get; set; }
+        public int[] Monitors { get; set; }
     }
     public class EnvironmentAdd
     {
@@ -34,8 +36,8 @@ namespace QIQO.Monitor.Api
     {
         public int ServerKey { get; set; }
         public string ServerName { get; set; }
-        public List<ServiceUpdate> Services { get; set; } = new List<ServiceUpdate>();
-        public List<EnvironmentUpdate> Environments { get; set; } = new List<EnvironmentUpdate>();
+        public int[] Services { get; set; }
+        public int[] Environments { get; set; }
     }
     public class ServiceUpdate
     {
